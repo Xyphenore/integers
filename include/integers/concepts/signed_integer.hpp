754ad44d84b53f85c22781229f05504c1b77faf7
Xyphenore <53941443+xyphenore@users.noreply.github.com>
2022-10-01 18:47:49 +0200

    🎨 Format

--- include/integers/concepts/signed_integer.hpp
+++ include/integers/concepts/signed_integer.hpp
@@ -4,14 +4,14 @@
 #include <concepts>
 
 namespace integers::concepts {
-    template<class Object>
-    concept SignedObject = std::is_signed_v<Object>;
+template <class Object>
+concept SignedObject = std::is_signed_v<Object>;
 
-    template<class Object>
-    concept IntegralObject = std::is_integral_v<Object>;
+template <class Object>
+concept IntegralObject = std::is_integral_v<Object>;
 
-    template<class IntegerType>
-    concept SignedInteger = IntegralObject<IntegerType> && SignedObject<IntegerType>;
+template <class IntegerType>
+concept SignedInteger = IntegralObject<IntegerType> && SignedObject<IntegerType>;
 
 } // namespace integers::concepts
 
